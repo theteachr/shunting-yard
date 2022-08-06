72c33ebd12a6f7a789c87d4b1f9b28f5e634b8bf
theteachr <sop.theteachr@gmail.com>
2022-08-06 21:45:47 +0530

    Concisify comment

--- src/lib.rs
+++ src/lib.rs
@@ -203,7 +203,7 @@ fn pop_until_left_paren(
 }
 
 fn handle_operation_parsing(op: Operator, output: &mut Vec<OutToken>, ops: &mut Vec<OpStackToken>) {
-	// While the operator at the top of the operator stack has a higher precedence than `op`,
+	// While the top of the operator stack has a higher precedence than `op`,
 	// pop it off and push it to the output queue.
 	while ops.last().map(|top| top.precedes(op)).unwrap_or(false) {
 		let op = ops.pop().and_then(|op| op.try_into().ok()).unwrap();
